bb43eba6143bc6d1eae0e37f461ba4918bd19be4
vincelegros <vincent.legros08@gmail.com>
2017-04-19 13:08:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ module.exports = new Script({
     },
 
     askName: {
-        prompt: (bot) => bot.say('What's your name pretty lady?'),
+        prompt: (bot) => bot.say('What\'s your name pretty lady?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
